1bbe196b9684c376cfaf5b0f81a26dccfc216370
thechaos <villaclara1231@gmail.com>
2023-06-19 00:51:53 +0300

    moving on to final design

---
+++ WordsTeacher.Application/Words/DeleteWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordsTeacher.DB;
+
+namespace WordsTeacher.Application.Words
+{
+	public class DeleteWord
+	{
+		private readonly ApplicationContext _context;
+		
+		public DeleteWord(ApplicationContext context)
+		{
+			_context = context;
+		}
+
+
+		public void Do(string word, string nick)
+		{
+			// to do deletion word. maybe with other parameters
+		}
+	}
+}
