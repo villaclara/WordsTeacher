c42d29c489525524427c32a08197740cdc6696f6
thechaos <villaclara1231@gmail.com>
2023-07-25 23:10:35 +0300

    customizing tab. added change password fields.

--- WordsTeacher.UI/Pages/Account.cshtml.cs
+++ WordsTeacher.UI/Pages/Account.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WordsTeacher.DB;
 
 namespace WordsTeacher.UI.Pages
 {
     public class AccountModel : PageModel
     {
+        private readonly ApplicationContext _context;
+        public int WordsPerUser { get; set; }
+
+        public AccountModel(ApplicationContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            WordsPerUser = _context.Words.Where(w => w.NickName == HttpContext.User.Identity!.Name).Count();
         }
-    }
+
+        public void OnPostChangePassword()
+        {
+
+        }
+
+	}
 }
